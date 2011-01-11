e899e3e5f111d6ddfad6dd98c0dd58a14717a66a
Douglas Bagnall <douglas@paradise.net.nz>
2011-01-11 14:47:01 +1300

    Write initial gtk app, based on teh internets

---
+++ wha.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+import pygst
+pygst.require("0.10")
+import gst
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+WIDTH = 320
+HEIGHT = 240
+
+class Screen:
+    def __init__(self, videosrc=None):
+        #video screen
+        self.screen = gtk.DrawingArea()
+        self.screen.set_size_request(WIDTH, HEIGHT)
+
+        #pack window
+        self.window = gtk.Window()
+        self.vbox = gtk.VBox()   # vertical box
+        self.vbox.pack_start(self.screen)
+        self.vbox.pack_start(self.buttons)
+        self.window.add(self.vbox)
+
+        # Create GStreamer bits and bobs
+        self.videosrc = videosrc        
+        self.pipeline = gst.Pipeline()        
+        self.pipeline.add(self.videosrc)
+        self.sink = gst.element_factory_make("ximagesink", "sink")
+        self.pipeline.add(self.sink)
+        self.videosrc.link(self.sink)
+
+        self.window.show_all()
+        self.realise()
+        self.sink.set_xwindow_id(self.screen.window.xid)
+        self.pipeline.set_state(gst.STATE_PLAYING)
+
+
+class Main:
+    def __init__(self):
+        #video screen
+        self.screen = gtk.DrawingArea()
+        self.screen.set_size_request(WIDTH, HEIGHT)
+
+        #buttons
+        self.play_button = gtk.Button(stock=gtk.STOCK_MEDIA_PLAY)
+        self.play_button.connect("clicked", self.play_onclick)
+        self.stop_button = gtk.Button(stock=gtk.STOCK_MEDIA_STOP)
+        self.stop_button.connect("clicked", self.stop_onclick)
+        self.quit_button = gtk.Button(stock=gtk.STOCK_QUIT)
+        self.quit_button.connect("clicked", self.quit_onclick)
+
+        self.buttons = gtk.HButtonBox()
+        self.buttons.add(self.play_button)
+        self.buttons.add(self.stop_button)
+        self.buttons.add(self.quit_button)
+
+        #pack window
+        self.window = gtk.Window()
+        self.vbox = gtk.VBox()   # vertical box
+        self.vbox.pack_start(self.screen)
+        self.vbox.pack_start(self.buttons)
+        self.window.add(self.vbox)
+
+        # Create GStreamer bits and bobs
+        self.pipeline = gst.Pipeline("mypipeline")
+        self.videosrc = gst.element_factory_make("videotestsrc", "video")
+        self.pipeline.add(self.videosrc)
+        self.sink = gst.element_factory_make("ximagesink", "sink")
+        self.pipeline.add(self.sink)
+        self.videosrc.link(self.sink)
+
+        #
+        #self.window.connect("delete_event", self.delete_event)
+        self.window.connect("destroy", self.destroy)
+
+        self.window.show_all()
+
+    def play_onclick(self, widget):
+        print "play"
+        self.sink.set_xwindow_id(self.screen.window.xid)
+        self.pipeline.set_state(gst.STATE_PLAYING)
+
+    def stop_onclick(self, widget):
+        print "stop"
+        self.pipeline.set_state(gst.STATE_READY)
+
+    def destroy(self, widget, data=None):
+        print "bye"
+        gtk.main_quit()
+
+    quit_onclick = destroy
+
+start = Main()
+gtk.main()
