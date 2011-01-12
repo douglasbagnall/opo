4bf82c1e87501a1eab7ef51affb0072e499c80ab
Douglas Bagnall <douglas@paradise.net.nz>
2011-01-12 17:03:53 +1300

    C version tries to split up video between screens

--- gtk-app.c
+++ gtk-app.c
@@ -10,36 +10,59 @@
 
 static void
 post_tee_pipeline(GstPipeline *pipeline, GstElement *tee, GstElement *sink,
-		  int id){
+    int crop_left, int crop_right){
   GstElement *queue = gst_element_factory_make("queue", NULL);
+  GstElement *crop = gst_element_factory_make("videocrop", NULL);
+
+  g_object_set(G_OBJECT(crop),
+      "top", 0,
+      "bottom", 0,
+      "left", crop_left,
+      "right", crop_right,
+      NULL);
+
   gst_bin_add_many (GST_BIN(pipeline),
       queue,
+      crop,
       sink,
       NULL);
 
   gst_element_link_many(tee,
       queue,
+      crop,
       sink,
       NULL);
 }
 
 static GstElement *
-pre_tee_pipeline(GstPipeline *pipeline){
+pre_tee_pipeline(GstPipeline *pipeline, int width, int height){
   if (pipeline == NULL){
     pipeline = GST_PIPELINE(gst_pipeline_new("wha_pipeline"));
   }
   char * src_name = (option_fake) ? "videotestsrc" : "v4l2src";
   GstElement *src = gst_element_factory_make(src_name, NULL);
   GstElement *tee = gst_element_factory_make ("tee", NULL);
+  GstCaps *caps = gst_caps_new_any();
+
+  GValue gw = {0};
+  GValue gh = {0};
+  g_value_init(&gw, G_TYPE_INT);
+  g_value_init(&gh, G_TYPE_INT);
+  g_value_set_int(&gw, width);
+  g_value_set_int(&gh, height);
+
+  gst_caps_set_value(caps, "width", &gw);
+  gst_caps_set_value(caps, "height", &gh);
 
   gst_bin_add_many(GST_BIN(pipeline),
       src,
+      caps,
       tee,
       NULL);
 
-  gst_element_link_many(src,
+  gst_element_link_filtered(src,
       tee,
-      NULL);
+      caps);
   return tee;
 }
 
@@ -57,11 +80,12 @@ static GstPipeline *
 make_multi_pipeline(windows_t *windows, int count)
 {
   GstPipeline *pipeline = GST_PIPELINE(gst_pipeline_new("e_wha"));
-  GstElement *tee = pre_tee_pipeline(pipeline);
+  GstElement *tee = pre_tee_pipeline(pipeline, windows->input_width, option_height);
   int i;
   for (i = 0; i < count; i++){
-    GstElement *sink = windows->sinks[i];
-    post_tee_pipeline(pipeline, tee, sink, i);
+    window_t *w = &windows->windows[i];
+    GstElement *sink = w->sink;
+    post_tee_pipeline(pipeline, tee, sink, w->crop_left, w->crop_right);
   }
   return pipeline;
 }
@@ -78,10 +102,10 @@ bus_call(GstBus * bus, GstMessage *msg, gpointer data)
         windows->prepared, option_screens);
     if (windows->prepared == option_screens){
       for (int i = 0; i < option_screens; i++){
-        gst_x_overlay_set_xwindow_id(GST_X_OVERLAY(windows->sinks[i]),
-            windows->xwindows[i]);
-        g_print("connected sink %d to window %lu\n", i, windows->xwindows[i]);
-        hide_mouse(windows->gtk_windows[i]);
+        window_t *w = &windows->windows[i];
+        gst_x_overlay_set_xwindow_id(GST_X_OVERLAY(w->sink), w->xid);
+        g_print("connected sink %d to window %lu\n", i, w->xid);
+        hide_mouse(w->widget);
       }
     }
   }
@@ -129,8 +153,9 @@ video_widget_realize_cb(GtkWidget *widget, gpointer data)
   windows_t *windows = (windows_t *)data;
   int r = windows->realised;
   if (r < MAX_SCREENS){
-    windows->xwindows[r] = GDK_WINDOW_XID(GDK_WINDOW(widget->window));
-    g_print("realised window %d with XID %lu\n", r, windows->xwindows[r]);
+    window_t *w = &windows->windows[r];
+    w->xid = GDK_WINDOW_XID(GDK_WINDOW(widget->window));
+    g_print("realised window %d with XID %lu\n", r, w->xid);
   }
   else {
     g_print("wtf, there seem to be %d windows!\n", r);
@@ -143,7 +168,7 @@ video_widget_realize_cb(GtkWidget *widget, gpointer data)
 static void
 set_up_window(GMainLoop *loop, GtkWidget *window, int screen_no){
   static const GdkColor black = {0, 0, 0, 0};
-  gtk_window_set_default_size(GTK_WINDOW(window), WIDTH, HEIGHT);
+  gtk_window_set_default_size(GTK_WINDOW(window), option_width, option_height);
 
   if (option_fullscreen){
     gtk_window_fullscreen(GTK_WINDOW(window));
@@ -168,8 +193,7 @@ set_up_window(GMainLoop *loop, GtkWidget *window, int screen_no){
                                              gint y);
     */
     int x = (width / option_screens) * screen_no + 1;
-    gtk_window_move(GTK_WINDOW(window),
-		    x, 50);
+    gtk_window_move(GTK_WINDOW(window), x, 50);
     g_print("putting window %d at %d\n", screen_no, x);
   }
 
@@ -190,8 +214,14 @@ gstreamer_start(GMainLoop *loop)
 {
   windows_t windows;
   windows.realised = 0;
+  windows.requested = 0;
+  windows.prepared = 0;
+  windows.input_width = option_screens * option_width;
 
   int i;
+  int crop_left = 0;
+  int crop_right = windows.input_width - option_width;
+
   for (i = 0; i < option_screens; i++){
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     g_signal_connect(window, "realize",
@@ -199,8 +229,13 @@ gstreamer_start(GMainLoop *loop)
     /* set up sink here */
     GstElement *sink = gst_element_factory_make("ximagesink", NULL);
     set_up_window(loop, window, i);
-    windows.gtk_windows[i] = window;
-    windows.sinks[i] = sink;
+    window_t *w = &windows.windows[i];
+    w->widget = window;
+    w->sink = sink;
+    w->crop_left = crop_left;
+    w->crop_right = crop_right;
+    crop_left += option_width;
+    crop_right -= option_width;
   }
 
   GstElement *pipeline = (GstElement *)make_multi_pipeline(&windows, option_screens);
@@ -224,6 +259,7 @@ gint main (gint argc, gchar *argv[])
 {
   //initialise threads before any gtk stuff (because not using gtk_init)
   g_thread_init(NULL);
+  g_type_init();
   /*this is more complicated than plain gtk_init/gst_init, so that options from
     all over can be gathered and presented together.
    */
--- gtk-app.h
+++ gtk-app.h
@@ -1,12 +1,11 @@
-#define WIDTH  400
-#define HEIGHT 300
-
 #define QUOTE_(x) #x
 #define QUOTE(x) QUOTE_(x)
 
 static gboolean option_fake = TRUE; /* Should eventually be FALSE !*/
 static gboolean option_fullscreen = FALSE;
-static gint option_screens = 1;
+static gint option_screens = 3;
+static gint option_width = 320;
+static gint option_height = 320;
 
 #define MAX_SCREENS 8
 
@@ -15,18 +14,26 @@ static GOptionEntry entries[] =
   { "fake-source", 0, 0, G_OPTION_ARG_NONE, &option_fake,
     "use videotestsrc", NULL },
   { "full-screen", 'f', 0, G_OPTION_ARG_NONE, &option_fullscreen, "run full screen", NULL },
+  { "width", 'w', 0, G_OPTION_ARG_INT, &option_width, "width of each screen", NULL },
+  { "height", 'h', 0, G_OPTION_ARG_INT, &option_height, "height of screen", NULL },
   { "screens", 's', 0, G_OPTION_ARG_INT, &option_screens, "Use this many screens, (max "
     QUOTE(MAX_SCREENS) ")", "S" },
   { NULL, 0, 0, 0, NULL, NULL, NULL }
 };
 
+typedef struct window_s {
+  int crop_left;
+  int crop_right;
+  GstElement *sink;
+  XID xid;
+  GtkWidget *widget;
+} window_t;
 
 typedef struct windows_s {
   int realised;
   int requested;
   int prepared;
-  GstElement *sinks[MAX_SCREENS];
-  XID        xwindows[MAX_SCREENS];
-  GtkWidget  *gtk_windows[MAX_SCREENS];
+  int input_width;
+  window_t windows[MAX_SCREENS];
 } windows_t;
 
