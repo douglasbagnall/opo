90c89e0881075c1c38226e7ad6cf90093db469a7
Douglas Bagnall <douglas@paradise.net.nz>
2011-02-23 14:13:01 +1300

    test/pipeline takes filename argument

--- test/pipeline.py
+++ test/pipeline.py
@@ -7,6 +7,7 @@ OPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #test/opo_launcher is a symlink to ../opo-launcher
 import opo_launcher
 BASE_DIR='home/douglas/opo/'
+#opo_launcher.ENCODER = 'msmpeg4'
 
 TEST_VIDEOS = [
     OPO_DIR + '/test_video/SOCCER_352x288_30_avc_750.264.avi',
@@ -21,9 +22,10 @@ TEST_VIDEOS = [
     OPO_DIR + '/test_video/MOBILE_352x288_30_avc_384.264.avi',
     ]
 
+dest = (sys.argv + ['/tmp/test.avi'])[1]
 
 
-#p = opo_launcher.start_stitching_process('/tmp/test.avi', TEST_VIDEOS[:4], 352, 288)
-p = opo_launcher.start_stitching_process('/tmp/test.avi', TEST_VIDEOS[:4], 256, 192, 3)
+#p = opo_launcher.start_stitching_process(dest, TEST_VIDEOS[:4], 352, 288)
+p = opo_launcher.start_stitching_process(dest, TEST_VIDEOS[:4], 256, 192, 3)
 
 print p.communicate()
