9af3a2661456762f307a1b673e8f50d87a7900e6
Douglas Bagnall <douglas@paradise.net.nz>
2011-02-25 17:35:51 +1300

    make nhew.py work better

--- test/nhew.py
+++ test/nhew.py
@@ -5,9 +5,18 @@ import sys, os
 OPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #test/opo_launcher is a symlink to ../opo-launcher
+
+FORMAT = 'mpeg4'
+if len(sys.argv) > 1:
+    FORMAT =sys.argv[1]
+
 import opo_launcher
-opo_launcher.ENCODER = 'mpeg4'
+opo_launcher.ENCODER = FORMAT
 BASE_DIR='home/douglas/opo/'
+filename = './stitched_video/nhew-%s.avi' % FORMAT
+if os.path.exists(filename):
+    print "%s exists, exiting" % filename
+    sys.exit(1)
 
 TEST_VIDEOS = [           
     OPO_DIR + '/unstitched_video/Charmaine.mov',
@@ -17,6 +26,7 @@ TEST_VIDEOS = [
     ]
 
 #p = opo_launcher.start_stitching_process('/tmp/test.avi', TEST_VIDEOS[:4], 352, 288)
-p = opo_launcher.start_stitching_process('./stitched_video/nhew-mp4.avi', TEST_VIDEOS, 1024, 768, 2)
+p = opo_launcher.start_stitching_process(filename,
+                                         TEST_VIDEOS, 1024, 768, 2)
 
 print p.communicate()
