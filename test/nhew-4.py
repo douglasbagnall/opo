eedba1eea9ebabd5ea6e9e424a273cb870325aef
Douglas Bagnall <douglas@paradise.net.nz>
2011-03-01 11:24:29 +1300

    more nhew scripts; final one is nhew-4

---
+++ test/nhew-4.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys, os
+
+OPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#test/opo_launcher is a symlink to ../opo-launcher
+import opo_launcher
+
+try:
+    codec = sys.argv[1]
+except IndexError:
+    codec = 'mpeg4'
+try:
+    muxer = sys.argv[2]
+except IndexError:
+    muxer = 'avi'
+
+#SCALE = 3.0 / 4.0
+SCALE = 7.0 / 8.0
+
+
+filename = '%s/stitched_video/nhew2-512-%s-%s.%s' % (OPO_DIR, int(SCALE * 100), codec, muxer)
+
+INPUT_VIDEOS = [
+    OPO_DIR + '/unstitched_video/Charmaine 28th.mov',
+    OPO_DIR + '/unstitched_video/Julia resize 4 28th.mov',
+    OPO_DIR + '/unstitched_video/mum really finished.mov',
+    OPO_DIR + '/unstitched_video/luisa resize 4 28th.mov',
+    ]
+
+#p = opo_launcher.start_stitching_process('/tmp/test.avi', INPUT_VIDEOS[:4], 352, 288)
+p = opo_launcher.start_stitching_process(filename, INPUT_VIDEOS, 512, 384, 2,
+                                         encoder=codec, muxer=muxer, scale=SCALE,
+                                         clip_top=0, audio_codec='mp2')
+
+print p.communicate()
---
+++ test/nhew-shrink.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys, os
+
+OPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#test/opo_launcher is a symlink to ../opo-launcher
+import opo_launcher
+
+try:
+    codec = sys.argv[1]
+except IndexError:
+    codec = 'mpeg4'
+try:
+    muxer = sys.argv[2]
+except IndexError:
+    muxer = 'avi'
+
+
+
+filename = '%s/stitched_video/nhew-512-%s.%s' % (OPO_DIR, codec, muxer)
+
+INPUT_VIDEOS = [
+    OPO_DIR + '/unstitched_video/Charmaine.mov',
+    OPO_DIR + '/unstitched_video/Julia-ready-for-view.mov',
+    OPO_DIR + '/unstitched_video/Julie-ready-to-view-wif-sound.mov',
+    OPO_DIR + '/unstitched_video/Luisa.mov',
+    ]
+
+#p = opo_launcher.start_stitching_process('/tmp/test.avi', INPUT_VIDEOS[:4], 352, 288)
+p = opo_launcher.start_stitching_process(filename, INPUT_VIDEOS, 512, 384, 2,
+                                         encoder=codec, muxer=muxer, scale=1.0,
+                                         clip_top=0, audio_codec='mp2')
+
+print p.communicate()
