ad966f1e1711874826e635629d160c5a80deabd2
Douglas Bagnall <douglas@paradise.net.nz>
2011-03-10 17:12:56 +1300

    Add GPL3 headers to main files

--- opo.c
+++ opo.c
@@ -1,4 +1,21 @@
-/*N way video splitter */
+/* This file is part of Opo, a small Video Whale
+
+ Copyright (C) 2011 Douglas Bagnall
+
+ Opo is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free
+ Software Foundation, either version 3 of the License, or (at your
+ option) any later version.
+
+ Opo is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <gst/gst.h>
 #include <gtk/gtk.h>
 #include <gst/interfaces/xoverlay.h>
